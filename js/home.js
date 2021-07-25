95e62ca3441f79103eb4b31bec2802489b8f1275
SPAbhay <35527492+SPAbhay@users.noreply.github.com>
2021-07-25 10:49:02 +0530

    Add files via upload
    Added java script file for home page

---
+++ js/home.js
@@ -0,0 +1,22 @@
+
+const counters=document.querySelectorAll(".counter");
+// const speed=800;
+const time=50;
+counters.forEach(counter=>{
+    const updateCount = () =>{
+        const target=+counter.getAttribute("data-target");
+        const count=+counter.innerText;
+        // const speed=target/time;
+        const inc=target/time;
+
+        if (count<target)
+        {
+            counter.innerHTML="<h3>"+Math.ceil(count+inc)+"</h3>";
+            setTimeout(updateCount, 1*time);
+        }else{
+            counter.innerHTML="<h3>"+target+"+</h3>";
+        }
+    }
+
+    updateCount();
+})
